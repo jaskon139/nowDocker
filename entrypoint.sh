ce7f7bcc979bffddb93ae6a6a8bcafc76516b082
jaskon139 <jaskon139@gmail.com>
2018-09-24 12:52:38 +0000

    sdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,13 @@
 #cd /opt/wetty/ && node app.js -p $PORT
 #kcperserver server_linux_amd64 
 cd /app
-chmod +x /app/shadowsocks-server-linux64
+chmod +x /app/shadowsocks-server-linux64-1.1.5
 resultip=$(ifconfig eth0 |grep "inet addr"| cut -f 2 -d ":"|cut -f 1 -d " ")
-./shadowsocks-server-linux64-1.1.5 -c config.json &
 ./client_s -t 127.0.0.1:8388 -l :3824 --mode fast2&
 ./kcptunserver 10.241.62.73 9999 $resultip $resultip 3824&
-/app/gotty --port 8080 --permit-write --reconnect /bin/sh
+#./shadowsocks-server-linux64-1.1.5 -c config.json &
+/app/gotty --address 127.0.0.1 --port 8080 --permit-write --reconnect /bin/sh &
+/app/v2ray-v3.31-linux-64/v2ray -config /app/v2ray-v3.31-linux-64/config.json &
+cd /app/proxy 
+node server.js http://127.0.0.1:10000
 #cd /opt/wetty && /usr/bin/node app.js -p $PORT
