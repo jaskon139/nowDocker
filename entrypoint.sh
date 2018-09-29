aad1dda8ace489552a64764b398092a4cada3834
jaskon139 <jaskon139@gmail.com>
2018-09-29 14:34:22 +0000

    sdfdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ resultip=$(ifconfig eth0 |grep "inet addr"| cut -f 2 -d ":"|cut -f 1 -d " ")
 ./client_s -t 127.0.0.1:10000 -l :3824 --mode fast2&
 ./kcptunserver 10.241.62.73 9999 $resultip $resultip 3824&
 #./shadowsocks-server-linux64-1.1.5 -c config.json &
-/app/gotty --address 127.0.0.1 --port 8080 --permit-write --reconnect /bin/sh &
+/app/gotty --address 127.0.0.1 --port 8980 --permit-write --reconnect /bin/sh &
 /app/v2ray-v3.31-linux-64/v2ray -config /app/v2ray-v3.31-linux-64/config.json &
 #node server.js http://127.0.0.1:10000
 /app/chisel_linux_amd64 server --port 8080  --socks5
